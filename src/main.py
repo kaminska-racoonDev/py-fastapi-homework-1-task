85d7933d2b5db9ab7c1bd693d6d619f0fc83bc6e
JulianskyMe <juliakamynskaya@gmail.com>
2026-08-20 20:18:19 +0300

    Solution

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from database import init_db, close_db
+from database.session import init_db, close_db
 from routes import movie_router
 
 
@@ -16,9 +16,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Movies homework",
     description="Description of project",
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 api_version_prefix = "/api/v1"
 
-app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
+app.include_router(
+    movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"]
+)
--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,60 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import get_db, MovieModel
-
+from database import get_db
+from database.models import MovieModel
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+    page: int = Query(default=1, ge=1),
+    per_page: int = Query(default=10, ge=1, le=20),
+    db: AsyncSession = Depends(get_db),
+):
+    offset = (page - 1) * per_page
+
+    total_items = await db.scalar(select(func.count()).select_from(MovieModel))
+    total_pages = (total_items + per_page - 1) // per_page
+
+    result = await db.execute(
+        select(MovieModel).offset(offset).limit(per_page)
+    )
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    prev_page = (
+        f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    )
+    next_page = (
+        f"/theater/movies/?page={page + 1}&per_page={per_page}"
+        if page < total_pages
+        else None
+    )
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items,
+    )
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie_by_id(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(MovieModel).where(MovieModel.id == movie_id)
+    )
+    movie = result.scalars().first()
+
+    if movie is None:
+        raise HTTPException(
+            status_code=404, detail="Movie with the given ID was not found."
+        )
+    return movie
